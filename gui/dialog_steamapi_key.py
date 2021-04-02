89764d517ac0926999971c23dda10f6a5abadd54
Tommi Saira <tommi@saira.fi>
2021-04-02 17:43:37 +0300

    Indent lines with 2 spaces instead of 4

--- gui/dialog_steamapi_key.py
+++ gui/dialog_steamapi_key.py
@@ -3,47 +3,47 @@ from PySide2.QtWidgets import QDialog, QVBoxLayout, QLabel, QLineEdit, QPushButt
 from _i18n import _
 
 class DialogSteamapiKey:
-    steamapi_window: QDialog
+  steamapi_window: QDialog
 
-    def is_valid_steampi_key(self, key):
-        if len(key) == 32:
-            return True
-        return False
+  def is_valid_steampi_key(self, key):
+    if len(key) == 32:
+      return True
+    return False
 
 
-    def steamapi_key_dialog(self):
-        self.steamapi_window = QDialog()
-        self.steamapi_window.setWindowTitle(_("Set steamapi key"))
-        self.steamapi_window.setWindowIcon(self.switcher_logo)
+  def steamapi_key_dialog(self):
+    self.steamapi_window = QDialog()
+    self.steamapi_window.setWindowTitle(_("Set steamapi key"))
+    self.steamapi_window.setWindowIcon(self.switcher_logo)
 
-        layout = QVBoxLayout()
-        self.steamapi_window.setLayout(layout)
+    layout = QVBoxLayout()
+    self.steamapi_window.setLayout(layout)
 
-        text_label = QLabel(
-            _("Used for getting avatars. Get yours from <a href='https://steamcommunity.com/dev/apikey'>steam</a>"))
-        apikey_edit = QLineEdit()
-        save_button = QPushButton(_("Save"))
+    text_label = QLabel(
+      _("Used for getting avatars. Get yours from <a href='https://steamcommunity.com/dev/apikey'>steam</a>"))
+    apikey_edit = QLineEdit()
+    save_button = QPushButton(_("Save"))
 
-        text_label.setOpenExternalLinks(True)
-        apikey_edit.setText(self.switcher.settings.get("steam_api_key"))
+    text_label.setOpenExternalLinks(True)
+    apikey_edit.setText(self.switcher.settings.get("steam_api_key"))
 
-        layout.addWidget(text_label)
-        layout.addWidget(apikey_edit)
-        layout.addWidget(save_button)
+    layout.addWidget(text_label)
+    layout.addWidget(apikey_edit)
+    layout.addWidget(save_button)
 
-        def save_enabled():
-            save_button.setEnabled(self.is_valid_steampi_key(apikey_edit.text()))
+    def save_enabled():
+      save_button.setEnabled(self.is_valid_steampi_key(apikey_edit.text()))
 
-        def save():
-            self.switcher.settings["steam_api_key"] = apikey_edit.text()
-            self.switcher.settings_write()
-            self.steamapi_window.hide()
-            if self.switcher.first_run:
-                self.import_accounts_dialog()
+    def save():
+      self.switcher.settings["steam_api_key"] = apikey_edit.text()
+      self.switcher.settings_write()
+      self.steamapi_window.hide()
+      if self.switcher.first_run:
+        self.import_accounts_dialog()
 
-        save_enabled()
+    save_enabled()
 
-        apikey_edit.textChanged.connect(lambda: save_enabled())
-        save_button.clicked.connect(lambda: save())
+    apikey_edit.textChanged.connect(lambda: save_enabled())
+    save_button.clicked.connect(lambda: save())
 
-        self.steamapi_window.show()
+    self.steamapi_window.show()
