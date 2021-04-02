57e8c947542324137c2c7f7d388941a75f337123
Tommi Saira <tommi@saira.fi>
2021-04-02 20:24:44 +0300

    Trying to fix broken test by changing setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
+from setuptools import setup
+
+with open("readme.md", 'r') as f:
+    long_description = f.read()
+
 setup(
     name="steam_account_switcher",
     version="0.0.1",
     description="Program to quickly switch between steam accounts",
-    long_description=readme,
+    long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/tommis/steam_account_switcher",
+    download_url="https://github.com/tommis/steam_account_switcher/releases",
     author="Tommi Saira",
     author_email="tommi@saira.fi",
     license="MIT",
